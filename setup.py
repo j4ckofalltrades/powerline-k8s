b98dd48ad186fe7e2d9d4ed86d977d028048ea3d
Jordan Duabe <1442368+j4ckofalltrades@users.noreply.github.com>
2021-05-24 09:29:26 +0800

    Set lower minimum Python version

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='powerline-k8s',
-    description='A Powerline segment for showing the active Kubernetes context and namespace',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    version='1.0.0',
-    keywords='powerline k8s',
-    license='MIT',
-    author='Jordan Duabe',
-    author_email='jordan.duabe@gmail.com',
-    url='https://github.com/j4ckofalltrades/powerline-k8s',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        "Operating System :: OS Independent",
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Topic :: Terminals'
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.9",
-)
+setuptools.setup()
